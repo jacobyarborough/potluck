6ccd2dd40a01300fdd702b74bb8d019b2f764844
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-01 10:06:18 -0600

    Test: create #initialize test

---
+++ test/potluck_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require './lib/dish'
+require './potluck'
+
+
+describe Potluck do
+
+describe '#initialize' do
+  it "creates an instance with readable attributes" do
+    
+  end
+end
+
+
+end

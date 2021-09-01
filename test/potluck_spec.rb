df1d8ad85a09d926ca9ddfa9f72f5da2bd4ec10e
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-01 11:31:50 -0600

    Edit #initialize test

--- test/potluck_spec.rb
+++ test/potluck_spec.rb
@@ -1,13 +1,16 @@
 require 'rspec'
 require './lib/dish'
-require './potluck'
+require './lib/potluck'
 
 
 describe Potluck do
 
 describe '#initialize' do
   it "creates an instance with readable attributes" do
-    
+    potluck = Potluck.new("7-13-18")
+
+    expect(potluck.date).to eq("7-13-18")
+    expect(potluck.dishes).to eq([])
   end
 end
 

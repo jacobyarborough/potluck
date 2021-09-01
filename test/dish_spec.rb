12e44b4894251a1186afbdbd31875924d05b8aab
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-01 09:43:30 -0600

    Test: Refactor tests

--- test/dish_spec.rb
+++ test/dish_spec.rb
@@ -1,8 +1,23 @@
+require 'rspec'
+require './lib/dish'
+
 describe Dish do
 
   it "exists" do
-    dish = Dish.new
+    name = "Couscous Salad"
+    category = :appetizer
+    dish = Dish.new(name, category)
     expect(dish).to be_an_instance_of(Dish)
+  end
+
+  describe "#initialize" do
+    it "creates an instance object from class Dish with two attributes" do
+      name = "Couscous Salad"
+      category = :appetizer
+      dish = Dish.new(name, category)
+      expect(dish.name).to eq("Couscous Salad")
+      expect(dish.category).to eq(:appetizer)
+    end
   end
 
 end
